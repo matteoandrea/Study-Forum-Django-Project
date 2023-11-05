683ba495f6396da170594065c585069a1be8920e
Matteo Andrea <61761339+matteoandrea@users.noreply.github.com>
2023-11-05 13:34:45 +0100

    first commit

---
+++ base/views.py
@@ -0,0 +1,168 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, Message
+from .forms import RoomForm
+
+# Create your views here.
+
+def loginPage(request):
+
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request=request,message="User does not exist")
+        
+        user = authenticate(request=request, username=username, password=password)
+
+        if user is not None:
+            login(request=request,user=user) 
+            return redirect('home')
+        else:
+            messages.error(request=request,message="Username or Password does not exist")
+
+    context = {'page': page}
+    return render(request=request, template_name='base/login_register.html',context=context)
+
+def logoutUser(request):
+    logout(request=request)
+    return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request=request,user=user)
+            return redirect('home')
+        else:
+            messages.error(request=request,message="Error occurred during registration")
+
+    context = {'form':form}
+    return render(request=request, template_name= 'base/login_register.html',context=context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    
+    context = {
+        "user":user,
+        "rooms":rooms,
+        "room_messages":room_messages,
+        "topics":topics
+    }
+
+    return render(request=request, template_name='base/profile.html', context=context)
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q')!= None else ''
+    
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains= q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q) |
+        Q(host__username__icontains=q)
+    )
+    
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {
+        "rooms":rooms,
+        "topics":topics,
+        "room_count":room_count,
+        "room_messages":room_messages
+    }
+    
+    return  render(request=request, template_name='base/home.html', context=context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {
+        "room": room,
+        "room_messages":room_messages,
+        "participants":participants
+    }
+
+    return render(request=request,template_name= 'base/room.html',context=context)
+
+@login_required(login_url='login')
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+                room = form.save(commit=False)
+                room.host = request.user
+                room.save()
+
+                return redirect('home')
+
+    context = {'form':form}
+    return render(request=request,template_name='base/room_form.html', context=context) 
+
+@login_required(login_url='login')
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed')
+    
+    if  request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request= request, template_name= 'base/room_form.html',context= context)
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed')
+
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+    
+    return render(request=request,  template_name= 'base/delete.html', context={'obj':room})
